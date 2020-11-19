5f787bf43c932725270525bc87a4c8292575c1af
rntkym <shimizu.ran.0618@gmail.com>
2020-11-19 19:26:24 +0900

    バツが勝った時に「バツの勝ち！」と表示されない不具合を修正

--- JavaScript/tic-tac-toe/game.js
+++ JavaScript/tic-tac-toe/game.js
@@ -42,7 +42,7 @@ function game(input){
       squares[random1][random2] = "×";
       var pc = document.getElementById("" + random1 + random2);
       pc.innerHTML = "×";
-      return;
+      break;
     }
   }
   for(var i = 0; i < win.length; i++){
