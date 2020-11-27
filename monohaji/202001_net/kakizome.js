9f5065fa6ccf7f5335f22704c0b98d20c9fc6c33
rntkym <shimizu.ran.0618@gmail.com>
2020-11-27 12:48:35 +0900

    【ものはじ2020年1月N中ネット/HTML/CSS/JavaScript×今年の抱負】新規ファイルを追加

---
+++ monohaji/202001_net/kakizome.js
@@ -0,0 +1,5 @@
+function kakizome(){
+    var inputArea = document.getElementById('input-area');
+    var outputArea = document.getElementById('output-area');
+    outputArea.innerHTML = inputArea.value;
+}
