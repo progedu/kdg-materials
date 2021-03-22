d3ec7b7a4a0a1daa05bfc9f13e82b44f3c57eacc
rntkym <shimizu.ran.0618@gmail.com>
2021-03-22 17:49:16 +0900

    【JavaScript/鬼ごっこ】new Image()を毎フレーム呼ばないようにする

--- JavaScript/tag/game.js
+++ JavaScript/tag/game.js
@@ -9,17 +9,32 @@ var id = setInterval(draw, 10);
 var time = 60;
 var count = 0;
 
+var player = new Image();
+player.src = "image/player.png";
 var playerX = w / 2;
 var playerY = h / 2;
 
+var enemy1 = new Image();
+var enemy2 = new Image();
+var enemy3 = new Image();
+enemy1.src = "image/enemy1.png";
+enemy2.src = "image/enemy2.png";
+enemy3.src = "image/enemy3.png";
+var enemy = [enemy1, enemy2, enemy3];
 var enemyX = [0, w / 2 - 50, w - 100];
 var enemyY = [h /2 - 50, 0, h / 2 - 50];
-
-var itemX = [0, 0, 0];
-var itemY = [0, 0, 0];
-
 var enemyInitialX = [0, w / 2 - 50, w - 100];
 var enemyInitialY = [h /2 - 50, 0, h / 2 - 50];
+
+var item1 = new Image();
+var item2 = new Image();
+var item3 = new Image();
+item1.src = "image/item1.png";
+item2.src = "image/item2.png";
+item3.src = "image/item3.png";
+var item = [item1, item2, item3];
+var itemX = [0, 0, 0];
+var itemY = [0, 0, 0];
 
 function draw(){
   ctx.fillStyle = "#FAEFD4";
@@ -39,20 +54,15 @@ function draw(){
   ctx.font = "30px 'ＭＳ ゴシック'";
   ctx.fillStyle = "#FFFFFF";
   ctx.fillText("残り時間：" + time, 50, 50);
-
-  var player = new Image();
-  player.src = "image/player.png";
   ctx.drawImage(player, playerX, playerY, 100, 100);
 
   for(var i = 0; i < 3; i ++){
-    var enemy = new Image();
-    var enemyPath = "image/enemy" + (i + 1) + ".png";
-    enemy.src = enemyPath;
-    ctx.drawImage(enemy, enemyX[i], enemyY[i], 100, 100);
+    ctx.drawImage(enemy[i], enemyX[i], enemyY[i], 100, 100);
 
     if(i > Math.floor((60 - time) / 20)){
       continue;
     }
+
     var tilt = (playerY - enemyY[i]) / (playerX - enemyX[i]);
     var x = Math.sqrt(1 / (1 + tilt * tilt));
     if(playerX > enemyX[i]){
@@ -74,10 +84,7 @@ function draw(){
   }
 
   for(var i = 0; i < 3; i ++){
-    var item = new Image();
-    var itemPath = "image/item" + (i + 1) + ".png";
-    item.src = itemPath;
-    ctx.drawImage(item, itemX[i], itemY[i], 50, 50);
+    ctx.drawImage(item[i], itemX[i], itemY[i], 50, 50);
     if(count % 500 == 0){
       itemX[i] = Math.random() * ( w - 300 ) + 100;
       itemY[i] = Math.random() * ( h - 200 ) + 100;
